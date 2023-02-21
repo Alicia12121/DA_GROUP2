a6047de6d797d7c262aa8e74922daa0c4aacb9e1
Alicia12121 <aliciachan0808@gmail.com>
2023-02-21 11:07:03 +0800

    INITIAL COMMIT

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #Use the Request library
 import requests
 
-
 # Set the target webpage
 url = 'http://172.18.58.80/varsity/'
 webpage = requests.get(url)
@@ -16,15 +15,17 @@ print("\t *", webpage.status_code)
 # Get the headers
 h = requests.head(url)
 print("Header:")
-print("*********")
+print("**********")
+
 # To print line by line
 for x in h.headers:
     print("\t ", x, ":", h.headers[x])
-    print("*********")
+    print("**********")
 
 # Modify the headers user-agent
 headers = {'User-Agent': 'Mobile'}
 
+# Test it on an external site
 url2 = 'http://httpbin.org/headers'
-rh = requests.get(url2, headers=headers)
-print(rh.text)
+request_header = requests.get(url2, headers=headers)
+print(request_header.text)
